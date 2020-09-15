87adcc991f756a8de5b77fa3e7286400c5f57405
Dmg5th <Dmgould310@gmail.com>
2020-09-15 12:42:43 -0400

    Added sessions controller and started login page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,37 +1,50 @@
 class SessionsController < ApplicationController
 
-  # GET: /sessions
-  get "/sessions" do
-    erb :"/sessions/index.html"
+#   # GET: /sessions
+  get "/login" do
+    erb :"users/login.html"
   end
 
-  # GET: /sessions/new
-  get "/sessions/new" do
-    erb :"/sessions/new.html"
-  end
+#   # GET: /sessions/new
+#   get "/sessions/new" do
+#     erb :"/sessions/new.html"
+#   end
 
   # POST: /sessions
-  post "/sessions" do
-    redirect "/sessions"
+  post "/login" do
+    if user.username.empty? || user.password.empty?
+      @error = "Username and password can't be blank"
+      erb :"/users/login.html"
+    user = User.find_by(username: params["username"], password: params['password'])
+    
+      
+   
+    elsif User.find_by(username: user.username)
+      @error = "Account with that username already exists "
+    else 
+      user.save
+      session[:user_id] = user.id 
+      redirect '/'
+    end 
   end
 
-  # GET: /sessions/5
-  get "/sessions/:id" do
-    erb :"/sessions/show.html"
-  end
+#   # GET: /sessions/5
+#   get "/sessions/:id" do
+#     erb :"/sessions/show.html"
+#   end
 
-  # GET: /sessions/5/edit
-  get "/sessions/:id/edit" do
-    erb :"/sessions/edit.html"
-  end
+#   # GET: /sessions/5/edit
+#   get "/sessions/:id/edit" do
+#     erb :"/sessions/edit.html"
+#   end
 
-  # PATCH: /sessions/5
-  patch "/sessions/:id" do
-    redirect "/sessions/:id"
-  end
+#   # PATCH: /sessions/5
+#   patch "/sessions/:id" do
+#     redirect "/sessions/:id"
+#   end
 
-  # DELETE: /sessions/5/delete
-  delete "/sessions/:id/delete" do
-    redirect "/sessions"
-  end
+#   # DELETE: /sessions/5/delete
+#   delete "/sessions/:id/delete" do
+#     redirect "/sessions"
+#   end
 end
